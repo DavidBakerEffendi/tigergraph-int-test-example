df07102555046bddedba8625570fb2187eb3c760
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-01-11 20:35:56 +0200

    :sparkles: Have working example

--- app/src/test/java/io/github/davidbakereffendi/example/TigerAppIntTest.java
+++ app/src/test/java/io/github/davidbakereffendi/example/TigerAppIntTest.java
@@ -1,36 +1,79 @@
 package io.github.davidbakereffendi.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TigerAppIntTest {
-    @Test
-    void appHasAGreetingTest() {
+
+    private static final String DB_ENDPOINT = "http://localhost:9000";
+
+    @BeforeAll
+    static void setUpAll() {
         var client = HttpClient.newHttpClient();
+        var payload = TigerAppIntTest.class.getClassLoader().getResource("data/payload.json");
 
-        var request = HttpRequest.newBuilder(URI.create("https://httpbin.org/get"))
-                .header("accept", "application/json")
-                .build();
-
+        HttpRequest request;
         HttpResponse<String> response;
         try {
+            request = HttpRequest.newBuilder(URI.create(DB_ENDPOINT + "/graph/social"))
+                    .header("accept", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofFile(Path.of(payload.getPath())))
+                    .build();
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            // the response:
-            System.out.println(response.body());
+            // Test the response
             assertNotNull(response.body());
+            assertTrue(response.body().contains("\"results\":[{\"accepted_vertices\":2,\"accepted_edges\":1}]"));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        var client = HttpClient.newHttpClient();
+
+        HttpRequest request;
+        try {
+            request = HttpRequest.newBuilder(URI.create(DB_ENDPOINT + "/graph/social/vertices/person"))
+                    .header("accept", "application/json")
+                    .DELETE()
+                    .build();
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     @Test
-    void helloTest() {
-        System.out.println("Hello");
+    void helloQueryTest() {
+        var client = HttpClient.newHttpClient();
+
+        HttpRequest request;
+        HttpResponse<String> response;
+        try {
+            request = HttpRequest.newBuilder(URI.create(DB_ENDPOINT + "/query/hello?p=Dave"))
+                    .header("accept", "application/json")
+                    .GET()
+                    .build();
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // the response:
+            assertNotNull(response.body());
+            assertTrue(response.body().contains("\"results\":[{\"Result\":[{\"v_id\":\"Fred\",\"v_type\":\"person\",\"attributes\":{\"name\":\"Fred\",\"age\":63,\"gender\":\"Male\"}}]}]"));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
